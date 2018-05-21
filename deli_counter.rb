6a0a1281c047488533938423b487ab1ed398a69c
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 01:59:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |i|
+      katz_deli_new << i
+    puts "The line is currently: #{counter}. #{i } "
+    "The line is currently: 1. Ada 2. Grace 3. Kent"
