b74e1b83fd6dc734b27d79d7b9325a0b24084002
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 02:43:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,15 @@ def take_a_number(katz_deli, name)
 end
 
 
-def now_serving()
-
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    until katz_deli.length == 0
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift
+    end
+  end
 end
 
 #  def take_a_number(katz_deli, name)
