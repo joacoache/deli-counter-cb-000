2e2c63fd3c571966eb7029bc6663f3f187442435
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 02:19:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    (0..katz_deli.length).to_a.each do |i|
+    (0..(katz_deli.length - 1).to_a.each do |i|
     katz_deli[i] = "#{(i + 1)}. #{katz_deli[i]}"
     end
     katz_deli.join(" ")
     puts "The line is currently: #{katz_deli}"
-#    "The line is currently: 1. Ada 2. Grace 3. Kent"
    end
 end
