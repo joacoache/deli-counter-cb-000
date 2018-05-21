5f9095f0bc720ced8d362307b9caa877a016e6a2
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 02:15:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     (0..katz_deli.length).to_a.each do |i|
     katz_deli[i] = "#{(i + 1)}. #{katz_deli[i]} "
     end
-    katz_deli.join
+    katz_deli.join()
     puts "The line is currently: #{katz_deli}"
 #    "The line is currently: 1. Ada 2. Grace 3. Kent"
    end
