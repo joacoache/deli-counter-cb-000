fc484887871f9e8575767fc0c73dc56ed4c0570b
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 02:30:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,18 @@ def line(katz_deli)
     (0..(katz_deli.length - 1)).to_a.each do |i|
     katz_deli[i] = "#{(i + 1)}. #{katz_deli[i]}"
     end
-#    katz_deli.join(" ")
     puts "The line is currently: #{katz_deli.join(" ")}"
    end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.each do |i|
+  puts "Welcome, #{i}. You are number #{(katz_deli.index(i)) + 1}1 in line."
+  end
+end
+
+ Welcome, Ada. You are number 1 in line.
+
+def now_serving()
+
 end
